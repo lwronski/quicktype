75cf8d6811450824f204f4332b8a54473f3c29c9
Simon Parten <Quafadas@users.noreply.github.com>
2023-01-09 16:19:09 +0100

    Add some debugging flags

--- test/fixtures/scala3/run.sh
+++ test/fixtures/scala3/run.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-scala-cli circe.scala TopLevel.scala
+wget https://github.com/VirtusLab/scala-cli/releases/download/nightly/scala-cli-x86_64-pc-linux-static.gz
+gunzip scala-cli-x86_64-pc-linux-static.gz
+chmod +x scala-cli-x86_64-pc-linux-static
+./scala-cli-x86_64-pc-linux-static  -v -v -v circe.scala TopLevel.scala 
